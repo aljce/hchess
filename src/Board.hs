de829295aa3e57e8c5bc123d4701fb53e6e65b2a
Kyle McKean <mckean.kylej@gmail.com>
2015-12-19 14:11:39 -0500

    Added show instance for board

--- src/Board.hs
+++ src/Board.hs
@@ -1,59 +1,72 @@
-{-# LANGUAGE LambdaCase #-}
-module Board (Board(..), emptyBoard, parseBoard) where 
+{-# LANGUAGE TypeFamilies, RankNTypes #-}
+module Board (Board(..), emptyBoard) where 
 
 import Prelude hiding (takeWhile)
 
 import Data.Word
 import Data.Bits
-import Data.Attoparsec.ByteString.Char8
-import Control.Applicative
 
-data Board = Board {-# UNPACK #-} !Word64 --Black Piece Set 
-                   {-# UNPACK #-} !Word64 --White Piece Set
-                   {-# UNPACK #-} !Word64 --Pawns
-                   {-# UNPACK #-} !Word64 --Rooks 
-                   {-# UNPACK #-} !Word64 --Knights 
-                   {-# UNPACK #-} !Word64 --Bishops
-                   {-# UNPACK #-} !Word64 --Queens 
-                   {-# UNPACK #-} !Word64 --Kings
-                   deriving (Show)
+import Data.IntMap.Strict hiding (filter)
+
+import Data.Coerce
+
+class Color a where 
+        extractColor :: a -> Word64 
+
+instance Color BlackPieces where 
+        extractColor = unBC
+
+instance Color WhitePieces where 
+        extractColor = unWC
+
+newtype BlackPieces = BC { unBC :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype WhitePieces = WC { unWC :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype Pawns   = P { unP :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype Rooks   = R { unR :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype Knights = N { unN :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype Bishops = B { unB :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype Queens  = Q { unQ :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+newtype Kings   = K { unK :: Word64 } deriving (Bits,FiniteBits,Num,Show,Eq,Ord,Enum)
+
+data Board = Board {-# UNPACK #-} !BlackPieces 
+                   {-# UNPACK #-} !WhitePieces 
+                   {-# UNPACK #-} !Pawns 
+                   {-# UNPACK #-} !Rooks  
+                   {-# UNPACK #-} !Knights  
+                   {-# UNPACK #-} !Bishops
+                   {-# UNPACK #-} !Queens 
+                   {-# UNPACK #-} !Kings 
 
 emptyBoard :: Board 
-emptyBoard = Board 0 0 0 0 0 0 0 0 
+emptyBoard = Board 0 0 0 0 0 0 0 0   
+ 
+instance Show Board where 
+        showsPrec _ b = (showBoard b ++) 
 
-showWord64 :: Word64 -> String 
-showWord64 w = reverse $ map (\i -> if testBit w i then '1' else '0') [0..63]
+showBoard :: Board -> String  
+showBoard (Board bc wc p r n b q k) = (reverse . elems . unions . fmap toIntMap) bitBoards
+        where toIntMap :: (Word64,Word64,Char) -> IntMap Char 
+              toIntMap (mask,pieces,c) = 
+                (fromDistinctAscList . fmap (,c) . filter (testBit (mask .&. pieces))) [0..63] 
+              bitBoards = [(unBC bc,unP p,'p'),(unBC bc,unR r,'r'),(unBC bc,unN n,'n'),
+                           (unBC bc,unB b,'b'),(unBC bc,unQ q,'q'),(unBC bc,unK k,'k'),
+                           (unWC wc,unP p,'P'),(unWC wc,unR r,'R'),(unWC wc,unN n,'N'),
+                           (unWC wc,unB b,'B'),(unWC wc,unQ q,'Q'),(unWC wc,unK k,'K'),
+                           (-1,-1,' ')]
+
+showWord64 :: (FiniteBits a) => a -> String 
+showWord64 w = reverse $ fmap (\i -> if testBit w i then '1' else '0') [0..(finiteBitSize w - 1)]
 
 put8 :: String -> IO ()
 put8 [] = return ()
 put8 s = do
         putStrLn $ Prelude.take 8 s
         put8 $ Prelude.drop 8 s
-
-charUpdate :: Int -> Board -> Char -> Board 
-charUpdate i (Board bc wc p r n b q k) = \case
-        'p' -> Board (setBit bc i) wc (setBit p i) r n b q k 
-        'r' -> Board (setBit bc i) wc p (setBit r i) n b q k
-        'n' -> Board (setBit bc i) wc p r (setBit n i) b q k
-        'b' -> Board (setBit bc i) wc p r n (setBit b i) q k
-        'q' -> Board (setBit bc i) wc p r n b (setBit q i) k
-        'k' -> Board (setBit bc i) wc p r n b q (setBit k i)
-        'P' -> Board bc (setBit wc i) (setBit p i) r n b q k
-        'R' -> Board bc (setBit wc i) p (setBit r i) n b q k
-        'N' -> Board bc (setBit wc i) p r (setBit n i) b q k
-        'B' -> Board bc (setBit wc i) p r n (setBit b i) q k
-        'Q' -> Board bc (setBit wc i) p r n b (setBit q i) k
-        'K' -> Board bc (setBit wc i) p r n b q (setBit k i) 
-        ' ' -> Board bc wc p r n b q k
-
-
-parseBoard :: Parser Board 
-parseBoard = snd . toBoard . concat <$> many' (pieces <|> blank <|> slash) 
-        where toBoard :: String -> (Int,Board) 
-              toBoard = foldr (\c (i,b) -> (i+1,charUpdate i b c)) (0,emptyBoard)
-              pieces :: Parser String
-              pieces = (:[]) <$> satisfy (\c -> ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) 
-              blank :: Parser String
-              blank = (flip replicate ' ' . subtract 48 . fromEnum) <$> satisfy isDigit
-              slash = [] <$ char '/'
 
